e64af6abc16dc22c4b0d3ad2df1b4d346f0b0c99
Aml Fakhri <amlfakhry13@gmail.com>
2020-06-30 12:56:02 +0200

    |get allStudent&&allStudentPg&&getStudentById|

--- controller/students.js
+++ controller/students.js
@@ -136,5 +136,59 @@ router.post( "/login",
     }
   );
   
+
+  router.get("/allStudents", (req, res) => {
+    Students.find({}, function(err, data){
+        if(err){
+            console.log(err);
+            return
+        }
+        if(data.length == 0) {
+            console.log("No record found")
+            return
+        }
+        console.log(data);
+        res.status(200).json(data);
+    })
+})
   
+
+// all data Pagentiaed
+router.post('/allStudentsPg', function(req, res) {
+    console.log('page number : ' + req.body.page); 
+    console.log('per page : ' + req.body.perPage);
+    var pageNo = req.body.page ; // parseInt(req.query.pageNo)
+    var size = req.body.perPage;
+    var query = {}
+    if (pageNo < 0 || pageNo === 0) {
+        response = { "error": true, "message": "invalid page number, should start with 1" };
+        return res.json(response)
+    }
+    query.skip = size * (pageNo - 1)
+    query.limit = parseInt(size)
+    // Find some documents
+    Students.find({}, {}, query, function (err, data) {
+        // Mongo command to fetch all data from collection.
+        if (err) {
+            response = { "error": true, "message": "Error fetching data" };
+        } else {
+            response = { "error": false, "message": data };
+        }
+        res.json(response);
+    });
+});
+
+router.post("/getStudentById", (req, res) => {
+    let studentId =req.body.id
+    Students.find({_id : studentId}).populate({path:'courses',select:'name -_id'})
+   .then(student=>{
+       console.log(student);
+       res.status(200).json(student);
+   }).catch(err => {
+       console.log(err);
+       res.status(500).json({
+       message: 'NotDataFound'
+       });
+   })
+})
 module.exports = router;
