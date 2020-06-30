a463e53b5ceb7574d94b5a7b363376276545a2a5
Aml Fakhri <amlfakhry13@gmail.com>
2020-06-30 12:24:33 +0200

    |signuo student|

--- controller/students.js
+++ controller/students.js
@@ -2,42 +2,139 @@ const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-
+const { check, validationResult} = require("express-validator");
 const Students = require("../models/students");
 
-router.post('/register', (req, res) => {
-    const today = new Date();
-    const studentData = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        created: today
-    }
-    Students.findOne({
-        email: req.body.email
-    })
-    .then(student => {
-        if(!student){
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                studentData.password = hash
-                Students.create(studentData)
-                .then(student => {
-                    res.json({status: student.email + ' registered!'})
-                })
-                .catch(err => {
-                    res.send('error: ' + err)
-                })
-            })
-        } else {
-            res.json({error: ' student already exists'})
+
+router.post( "/register",
+    [
+        check("name", "Please Enter a Valid name")
+        .not()
+        .isEmpty(),
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "Please enter a valid password").isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
         }
-    })
-    .catch(err => {
-        res.send('error: ' + err)
-    })
-})
+
+        const {
+            name,
+            email,
+            password
+        } = req.body;
+        try {
+            let student = await Students.findOne({
+                email
+            });
+            if (student) {
+                return res.status(400).json({
+                    msg: "student Already Exists"
+                });
+            }
+
+            student = new Students({
+                name,
+                email,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            student.password = await bcrypt.hash(password, salt);
+
+            await student.save();
+
+            const payload = {
+                student: {
+                    id: student._id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                "randomString", {
+                    expiresIn: 10000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send("Error in Saving");
+        }
+    }
+);
 
 
-
-
+router.post( "/login",
+    [
+      check("email", "Please enter a valid email").isEmail(),
+      check("password", "Please enter a valid password").isLength({
+        min: 6
+      })
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        });
+      }
+  
+      const { email, password } = req.body;
+      try {
+        let student = await Students.findOne({
+          email
+        });
+        if (!student)
+          return res.status(400).json({
+            message: "student Not Exist"
+          });
+  
+        const isMatch = await bcrypt.compare(password, student.password);
+        if (!isMatch)
+          return res.status(400).json({
+            message: "Incorrect Password !"
+          });
+  
+        const payload = {
+            student: {
+            id: student._id
+          }
+        };
+  
+        jwt.sign(
+          payload,
+          "randomString",
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if (err) throw err;
+            res.status(200).json({
+              token
+            });
+          }
+        );
+      } catch (e) {
+        console.error(e);
+        res.status(500).json({
+          message: "Server Error"
+        });
+      }
+    }
+  );
+  
+  
 module.exports = router;
